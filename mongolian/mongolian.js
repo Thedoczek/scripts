402f94676a0ff0da306476cae1140159bc759637
Thedoczek <dokmiada@gmail.com>
2023-10-01 16:05:09 +0200

    alot

--- mongolian/mongolian.js
+++ mongolian/mongolian.js
@@ -1,10 +1,10 @@
-lat1 = ["o=", "u=", "q", "sh", "ch", "j"];
-lat2 = ["ö", "ü", "k", "š", "č", "ǰ"];
-lat = ["a", "e", "i", "o", "u", "ö", "ü", "ng", "n", "b", "p", "k", "g", "m", "l", "s", "š", "t", "d", "č", "ǰ", "y", "r"];
-mon = ["ᠠ", "ᠡ", "ᠢ", "ᠣ", "ᠤ", "ᠥ", "ᠦ", "ᠩ", "ᠨ", "ᠪ", "ᠫ", "ᠬ", "ᠭ", "ᠮ", "ᠯ", "ᠰ", "ᠱ", "ᠲ", "ᠳ", "ᠴ", "ᠵ", "ᠶ", "ᠷ"];
+lat1 = ["o=", "u=", "k", "qe", "qi", "qö", "qü", "g", "ɣe", "ɣi", "ɣö", "ɣü", "sh", "ch", "zh", "j", "v", "nɣ", "\\\\."];
+lat2 = ["ö", "ü", "q", "ke", "ki", "kö", "kü", "ɣ", "ge", "gi", "gö", "gü", "š", "č", "ž", "ǰ", "w", "ng", "."];
+lat = ["lh", "a", "e", "i", "o", "u", "ö", "ü", "ng", "n", "b", "p", "k", "q", "g", "ɣ", "m", "l", "s", "š", "t", "d", "č", "ǰ", "y", "r", "w", "f", "c", "z", "h", "ž", "-", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "\\.\\.\\.", ",", "\\.", ":"];
+mon = ["ᡀ", "ᠠ", "ᠡ", "ᠢ", "ᠣ", "ᠤ", "ᠥ", "ᠦ", "ᠩ", "ᠨ", "ᠪ", "ᠫ", "ᠬ", "ᠬ", "ᠭ", "ᠭ", "ᠮ", "ᠯ", "ᠰ", "ᠱ", "ᠲ", "ᠳ", "ᠴ", "ᠵ", "ᠶ", "ᠷ", "ᠸ", "ᠹ", "ᠼ", "ᠽ", "ᠾ", "ᠿ", "᠎", "᠐", "᠑", "᠒", "᠓", "᠔", "᠕", "᠖", "᠗", "᠘", "᠙", "᠁", "᠂", "᠃", "᠄"];
 
 function latmon() {
-	let car = document.form.leftarea.value;
+	let car = document.form.leftarea.value.toLowerCase();
 
 	// Convert special characters while keeping cursor position
 	for (let i = 0; i < lat1.length; ++i) {
@@ -35,6 +35,9 @@ function monlat() {
 	// Update latin side
 	for (let i = 0; i < lat.length; i++) {
 		car = car.replace(new RegExp(mon[i], "g"), lat[i]);
+	}
+	for (let i = 0; i < lat1.length; ++i) {
+		car = car.replace(new RegExp(lat1[i], "g"), lat2[i]);
 	}
 	document.form.leftarea.value = car;
 
