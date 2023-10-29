c647414e49375f3071ed8c13712a5630cb0e591d
Thedoczek <dokmiada@gmail.com>
2023-10-29 13:25:21 +0100

    it works for everything except "hangeul" lmao

--- hangul/hangul.js
+++ hangul/hangul.js
@@ -2,8 +2,8 @@ lat = ["ng", "g", "kk", "n", "d", "tt", "l", "m", "b", "pp", "ss", "s", "jj", "j
 han = ["ᆼ", "ᄀ", "ᄁ", "ᄂ", "ᄃ", "ᄄ", "ᄅ", "ᄆ", "ᄇ", "ᄈ", "ᄊ", "ᄉ", "ᄍ", "ᄌ", "ᄎ", "ᄏ", "ᄐ", "ᄑ", "ᄒ", "ᅤ", "ᅧ", "ᅫ", "ᅢ", "ᅣ", "ᅥ", "ᅨ", "ᅪ", "ᅬ", "ᅭ", "ᅯ", "ᅰ", "ᅱ", "ᅲ", "ᅳ", "ᅴ", "ᅴ", "ᅡ", "ᅦ", "ᅩ", "ᅮ", "ᅵ", "-"];
 typ = ["c", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "V", "V", "V", "V", "V", "V", "V", "V", "V", "V", "V", "V", "V", "V", "V", "V", "V", "V", "V", "V", "V", "V", " "];
 hanf = ["ᆼ", "ᆨ", "ᆩ", "ᆫ", "ᆮ", "ᄄ", "ᆯ", "ᆷ", "ᆸ", "ᄈ", "ᆻ", "ᆺ", "ᄍ", "ᆽ", "ᆾ", "ᆿ", "ᇀ", "ᇁ", "ᇂ"];
-han1 = ["ᆨᆺ", "ᆫᆽ", "ᆫᇂ", "ᆯᆨ", "ᆯᆷ", "ᆯᆸ", "ᆯᆺ", "ᆯᇀ", "ᆯᇁ", "ᆯᇂ", "ᆸᆸ", "-"];
-han2 = ["ᆪ", "ᆬ", "ᆭ", "ᆰ", "ᆱ", "ᆲ", "ᆳ", "ᆴ", "ᆵ", "ᆶ", "ᆹ", ""];
+han1 = ["ᆨᆺ", "ᆫᆽ", "ᆫᇂ", "ᆯᆨ", "ᆯᆷ", "ᆯᆸ", "ᆯᆺ", "ᆯᇀ", "ᆯᇁ", "ᆯᇂ", "ᆸᆸ"];
+han2 = ["ᆪ", "ᆬ", "ᆭ", "ᆰ", "ᆱ", "ᆲ", "ᆳ", "ᆴ", "ᆵ", "ᆶ", "ᆹ"];
 
 function lathan() {
 	// Convert letters to lowercase
@@ -62,20 +62,56 @@ function lathan() {
 	for (let i = 0; i < han1.length; ++i) {
 		car = car.replace(new RegExp(han1[i], "g"), han2[i]);
 	}
+	car = car.replace(new RegExp("-", "g"), "");
 
 	// run transcribe()
-	console.log(ccv);
+
 
 	document.form.rightarea.value = car;
 
 	autoResize(document.getElementById("leftarea"));
 	autoResize(document.getElementById("rightarea"));
-}
+}        // ㄱ    ㄲ    ㄴ    ㄷ    ㄸ    ㄹ    ㅁ    ㅂ    ㅃ    ㅅ    ㅆ    ㅇ    ㅈ    ㅉ    ㅊ    ㅋ    ㅌ    ㅍ    ㅎ
+initial = ['ᄀ', 'ᄁ', 'ᄂ', 'ᄃ', 'ᄄ', 'ᄅ', 'ᄆ', 'ᄇ', 'ᄈ', 'ᄉ', 'ᄊ', 'ᄋ', 'ᄌ', 'ᄍ', 'ᄎ', 'ᄏ', 'ᄐ', 'ᄑ', 'ᄒ'];
+medial = ['ᅡ', 'ᅢ', 'ᅣ', 'ᅤ', 'ᅥ', 'ᅦ', 'ᅧ', 'ᅨ', 'ᅩ', 'ᅪ', 'ᅫ', 'ᅬ', 'ᅭ', 'ᅮ', 'ᅯ', 'ᅰ', 'ᅱ', 'ᅲ', 'ᅳ', 'ᅴ', 'ᅵ'];
+final = ['', 'ᆨ', 'ᆩ', 'ᆪ', 'ᆫ', 'ᆬ', 'ᆭ', 'ᆮ', 'ᆯ', 'ᆰ', 'ᆱ', 'ᆲ', 'ᆳ', 'ᆴ', 'ᆵ', 'ᆶ', 'ᆷ', 'ᆸ', 'ᆹ', 'ᆺ', 'ᆻ', 'ᆼ', 'ᆽ', 'ᆾ', 'ᆿ', 'ᇀ', 'ᇁ', 'ᇂ'];
 
 function hanlat() {
 	// detect and decompose syllables into jamo
+	let car = document.form.rightarea.value.split('');
+	for (let i = 0; i < car.length; ++i) {
+		if (car[i].charCodeAt(0) >= 44032 && car[i].charCodeAt(0) <= 55203) {
+			let c = car[i].charCodeAt(0) - 44032; console.log(c);
+			let f = final[c % 28];
+			c = Math.floor(c/28); console.log(c);
+			let m = medial[c % 20];
+			c = Math.floor(c/20); console.log(c);
+			let j = initial[c % 16];
+			car[i] = j+m+f+"-";
+		}
+	}
+	car = car.join('');
+	console.log(car);
+
 	// run transcribe()
+	
+
 	// transliterate
+	for (let i = 0; i < han2.length; ++i) {
+		car = car.replace(new RegExp(han2[i], "g"), han1[i]);
+	}
+	for (let i = 0; i < hanf.length; ++i) {
+		car = car.replace(new RegExp(hanf[i], "g"), han[i]);
+	}
+	for (let i = 0; i < lat.length; ++i) {
+		car = car.replace(new RegExp(han[i], "g"), lat[i]);
+	}
+	car = car.replace(new RegExp("ᄋ", "g"), "");
+
+	document.form.leftarea.value = car;
+
+	autoResize(document.getElementById("leftarea"));
+	autoResize(document.getElementById("rightarea"));
 }
 
 function transcribe() {
